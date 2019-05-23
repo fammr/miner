5df5f03d425887057c502ee034474e3aed98a356
hejiaxuan7 <hejiaxuan7@sina.com>
2019-05-23 14:51:49 +0800

    first commit
    Merge remote-tracking branch 'git/master'
    
    
    精简配置文件结构
    
    
    修改
    
    
    修改 insert or update
    
    
    修改任务调用方式
    
    
    wer
    
    
    使用 Canal 获取 mysql boilog
    
    
    修改包名
    
    
    修改配置，在启动时加载
    
    
    完成
    
    
    完成同步删除操作
    
    
    修改数据同步的更新问题
    
    
    项目启动失败后直接退出
    
    
    取消使用future， 改为 Runnable
    
    
    失败重连，修改名称

---
+++ miner-server/src/main/java/com/hebaibai/miner/DataSourceConf.java
@@ -0,0 +1,13 @@
+package com.hebaibai.miner;
+
+
+import lombok.Data;
+
+@Data
+public class DataSourceConf {
+    private String name;
+    private String url;
+    private String username;
+    private String password;
+    private String driverClass;
+}
